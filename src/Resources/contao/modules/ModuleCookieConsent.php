c33e3ed2e5b459c6179388e5c631e0565ef4cf65
Christof Karisch <ckarisch@outlook.com>
2020-04-05 18:13:48 +0200

    save results to session, do not copy on reload

--- src/Resources/contao/modules/ModuleCookieConsent.php
+++ src/Resources/contao/modules/ModuleCookieConsent.php
@@ -10,6 +10,8 @@
 
 namespace Formundzeichen\ContaoCookieConsentBundle;
 
+use Contao\System;
+use Contao\Backend;
 
 /**
  * Class ModuleCookieConsent
@@ -22,6 +24,11 @@ class ModuleCookieConsent extends \BackendModule
      */
     protected $strTemplate = 'mod_cookie_consent';
 
+  	/**
+  	 * The session name
+  	 * @var string
+  	 */
+  	protected $strSession = 'fz_cookie_consent';
 
     /**
      * Function compile
@@ -30,9 +37,30 @@ class ModuleCookieConsent extends \BackendModule
     {
         $this->Template->results = array();
 
+    		$objSession = System::getContainer()->get('session');
+    		$arrSession = $objSession->get($this->strSession);
+
         switch( \Input::get('state') ) {
             case 'copy':
                 $this->Template->results = CopyTemplates::run( );
+                $arrSession['results'] = $this->Template->results;
+      					$objSession->set($this->strSession,$arrSession);
+
+                $this->redirect( Backend::addToUrl('state=copying-done') );
+          			return;
+
+            case 'copying-done':
+                // do not clear session
+
+                // show session variables
+                if($arrSession['results']) {
+                  $this->Template->results = $arrSession['results'];
+                }
+                break;
+
+            default:
+                // clear session on default
+                $objSession->set($this->strSession,[]);
                 break;
         }
 
