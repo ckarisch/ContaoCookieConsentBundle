8fb07a33fbe0736d1b9e3ce8e34c4fb4ea2e8c07
Christof Karisch <ckarisch@outlook.com>
2020-04-05 14:56:33 +0200

    update config

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -7,5 +7,21 @@
  * @license LGPL-3.0-or-later
  */
 
+ define('FZ_COOKIE_CONSENT_VERSION', '1.3');
+
 $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = ['Formundzeichen\ContaoCookieConsentBundle\EventListener\TemplateListener', 'onOutputFrontendTemplate'];
 $GLOBALS['TL_HOOKS']['getPageLayout'][] = ['Formundzeichen\ContaoCookieConsentBundle\EventListener\GetPageLayoutListener', 'onGetPageLayout'];
+// $GLOBALS['TL_HOOKS']['getUserNavigation'][] = ['formundzeichen_contao-cookie-consent-bundle.user_navigation_listener', 'onGetUserNavigation'];
+
+/**
+ * -------------------------------------------------------------------------
+ * BACK END MODULES
+ * -------------------------------------------------------------------------
+ */
+array_insert($GLOBALS['BE_MOD']['system'], -1, array
+(
+	'Cookies' => array (
+		'callback'   => 'Formundzeichen\ContaoCookieConsentBundle\ModuleCookieConsent',
+		'stylesheet' => 'bundles/contaocookieconsent/css/backend.min.css',
+	)
+));
