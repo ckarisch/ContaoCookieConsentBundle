8c17ccaf0ce78a3f8a94d0ca4e777ef97548ca00
Christof Karisch <ckarisch@outlook.com>
2020-07-08 17:35:55 +0200

    version 1.5.1

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -7,7 +7,7 @@
  *
  */
 
-define('FZ_COOKIE_CONSENT_VERSION', '1.5.0');
+define('FZ_COOKIE_CONSENT_VERSION', '1.5.1');
 
 $GLOBALS['TL_HOOKS']['outputFrontendTemplate'][] = ['Formundzeichen\ContaoCookieConsentBundle\EventListener\TemplateListener', 'onOutputFrontendTemplate'];
 $GLOBALS['TL_HOOKS']['getSystemMessages'][] = ['Formundzeichen\ContaoCookieConsentBundle\EventListener\GetSystemMessagesListener', 'onGetSystemMessages'];
